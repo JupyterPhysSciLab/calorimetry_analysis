0c4d8c589e74776e0c75d6d1ea3ec24d3a049a43
gutow <gutow@uwosh.edu>
2025-02-05 17:35:25 -0600

    Parr 6772 datalog.csv loader

--- calorimetry_analysis/utils.py
+++ calorimetry_analysis/utils.py
@@ -1,3 +1,6 @@
+import pandas
+
+
 def find_pandas_dataframe_names():
     """
     This operation will search the interactive name space for pandas
@@ -17,3 +20,34 @@ def find_pandas_dataframe_names():
         if not (str.startswith(k, '_')) and isinstance(global_dict[k], df):
             dataframenames.append(k)
     return dataframenames
+
+
+def parr6772_time_series_to_elapsed_time(times: pandas.Series) -> pandas.Series:
+    from pandas import Series
+
+    def parse_time(date_str):
+        from datetime import datetime
+        date = datetime.strptime(date_str, '%m/%d/%y %H:%M:%S')
+        timestamp = date.timestamp()
+        return timestamp
+
+    time_list = []
+    start_time = parse_time(str(times[0]))
+    for k in times:
+        time_list.append(parse_time(str(k)) - start_time)
+    return Series(time_list)
+
+def load_parr6772(datalog)-> pandas.DataFrame:
+    """Reads a Parr 6772 thermometer Datalog.csv file and returns a
+    pandas.DataFrame containing the columns "date", "Temp(C)" and "Time(s)",
+    where the last column is the elapsed time since the first datapoint in
+    the file.
+
+    :param datalog: String containing the datalog file name or path.
+    """
+    from pandas import read_csv
+
+    df = read_csv(datalog, header=None, usecols=[0,1], names = ["date",
+                                                                "Temp(C)"])
+    df["Time(s)"] = parr6772_time_series_to_elapsed_time(df["date"])
+    return df
